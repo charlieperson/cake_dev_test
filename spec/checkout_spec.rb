0b82a7b25e2e64bfa2b82136685e6e603944d6b1
charlieperson <charlie.person@hyperisland.se>
2016-05-20 14:52:49 +0100

    Adds failing test that checks if checkout can total everything in basket

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 require 'checkout'
 
 describe Checkout do
+
   subject(:checkout) { Checkout.new }
 
   it 'can add items to a basket' do
     checkout.scan(001)
     expect(checkout.basket).to eq [{:name=>"Lavender heart", :price=>9.25}]
+  end
+
+  it 'can add up the total of a basket' do
+    checkout.scan(001)
+    checkout.scan(002)
+    expect(checkout.total).to eq 54.25
   end
 end
