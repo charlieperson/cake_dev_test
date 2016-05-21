09d11585484a637a810ae2ca4c108803c1e455cd
charlieperson <charlie.person@hyperisland.se>
2016-05-21 12:27:05 +0100

    Adds functionality that drops price of lavender heart to 8.50 if more than one is purchased

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -17,6 +17,7 @@ class Checkout
   def total
     basket.each { |item| @total += item[:price] }
     over_sixty?
+    two_hearts?
     return @total
   end
 
@@ -25,5 +26,13 @@ class Checkout
 
   def over_sixty?
     @total > 60 ? @total = @total * 0.9 : @total
+  end
+
+  def two_hearts?
+    hearts = 0
+    basket.each do |item|
+      item[:name] == 'Lavender heart' ? hearts += 1 : hearts += 0
+    end
+    hearts > 1 ? @total -= (hearts * 0.75) : @total
   end
 end
